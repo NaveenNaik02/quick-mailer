08bb08d66e7a2f6d4c824818397d8d0fd150c633
Naveen <naveennaik0202@gmail.com>
2024-09-15 10:36:03 +0530

    production debug

--- service/google-spreadsheet.ts
+++ service/google-spreadsheet.ts
@@ -2,28 +2,32 @@
 import { google } from "googleapis";
 
 export async function getSheetData() {
-  const glAuth = await google.auth.getClient({
-    projectId: "lyrical-epigram-435516-c5",
-    credentials: {
-      type: "service_account",
-      project_id: "lyrical-epigram-435516-c5",
-      private_key_id: "1a512d9cde4842432bdca08b66d6929ae4ce730f",
-      private_key: process.env.GOOGLE_PRIVATE_KEY,
-      client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-      universe_domain: "googleapis.com",
-    },
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-  });
+  try {
+    const glAuth = await google.auth.getClient({
+      projectId: "lyrical-epigram-435516-c5",
+      credentials: {
+        type: "service_account",
+        project_id: "lyrical-epigram-435516-c5",
+        private_key_id: "1a512d9cde4842432bdca08b66d6929ae4ce730f",
+        private_key: process.env.GOOGLE_PRIVATE_KEY,
+        client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+        universe_domain: "googleapis.com",
+      },
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    });
 
-  const glSheets = google.sheets({ version: "v4", auth: glAuth });
+    const glSheets = google.sheets({ version: "v4", auth: glAuth });
 
-  console.log(process.env.GOOGLE_SHEET_ID, "sheet id");
+    console.log(process.env.GOOGLE_SHEET_ID, "sheet id");
 
-  const data = await glSheets.spreadsheets.values.get({
-    spreadsheetId: process.env.GOOGLE_SHEET_ID,
-    range: "SLE details!B:J",
-  });
+    const data = await glSheets.spreadsheets.values.get({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range: "SLE details!B:J",
+    });
 
-  console.log(data, "response");
-  return { data: data.data.values };
+    console.log(data, "response");
+    return { data: data.data.values };
+  } catch (error) {
+    console.log(error, "error");
+  }
 }
